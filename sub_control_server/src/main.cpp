9dddb568a341bb5066b9aceb71a3cb3250d15044
Baebird <dapperwbs@gmail.com>
2026-08-07 16:22:16 -0400

    Added ADS1015 and MCP23017 intitialization to main

--- sub_control_server/src/main.cpp
+++ sub_control_server/src/main.cpp
@@ -1,18 +1,42 @@
 #include <Arduino.h>
+#include <vector>
+#include <string>
+#include <sstream>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <Adafruit_ADS1X15.h>
+#include <Adafruit_MCP23X17.h>
+
+Adafruit_ADS1015 adc;
+Adafruit_MCP23X17 dgpio;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  adc.begin();
+  dgpio.begin_I2C();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+/*
+
+
+void setup()
+{
+  Serial.begin(115200);
 }
+
+void loop()
+{
+}
+*/
+
+// Thruster control: THRUST <right: -1, 1> <forward: -1, 1> <up: -1, 1> <look_right: -1, 1> <look_up: -1, 1>
+// Camera control: CAMERA MODE <day, night>
+// Camera control: CAMERA MODE TOGGLE
+// Lights control: LIGHTS <off/on>
+// Lights control: LIGHTS TOGGLE
+
+// Data sources:
+// IMU, pressure sensor, temp sensor, battery level sensor, battery draw sensor
